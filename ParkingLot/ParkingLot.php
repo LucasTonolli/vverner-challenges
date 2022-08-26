f998ac4094864c655d9a1dfaa44f2adf9b6e7311
LucasTonolli <tonollilucas@yahoo.com>
2022-08-26 12:30:13 -0300

    Alterações de funcionalidades

--- ParkingLot/ParkingLot.php
+++ ParkingLot/ParkingLot.php
@@ -6,28 +6,49 @@
      private $qtdVehiclesInParkingLot;  
      private $invoicing; 
      
-     function __construct(int $parkingLots, float $valueToEnter = 0) 
+     function __construct(int $parkingLots = 0, float $valueToEnter = 0) 
      {  
-         $this->parkingLots = $parkingLots; 
+         if ($parkingLots < 1) :
+            echo "Você tentou criar um estacionamento com menos de uma vaga!<br>";
+            echo "Será criado um estacionamento com 1 vaga, para alterar isso faça uma expansão.";
+            $this->parkingLots =  1;
+         else:
+            $this->parkingLots = $parkingLots;
+         endif;
          $this->valueToEnter = $valueToEnter === 0.00 ?  5.00 : $valueToEnter; 
          $this->invoicing = 0; 
          $this->qtdVehiclesInParkingLot = 0; 
      }  
   
-     public function getQtdOfParkingLots():int  
+     private function getQtdOfParkingLots():int  
      {  
          return $this->parkingLots;  
      }  
+     public function limitOfVehicles():void
+     {
+        $limitOfVehicles = $this->getQtdOfParkingLots();
+        echo "O limite do estacionamento é $limitOfVehicles veículo(s)";
+     }
       
      private function getValueToEnter():float 
      { 
          return $this->valueToEnter; 
      } 
    
-     public function getQtdOfVehiclesInParkingLot():int  
+     private function getQtdOfVehiclesInParkingLot():int  
      {  
          return $this->qtdVehiclesInParkingLot;  
      }  
+     public function howManyVehiclesAreInParkingLot():void
+     {
+        $vehiclesInParkingLot = $this->getQtdOfVehiclesInParkingLot(); 
+        if ($vehiclesInParkingLot !== 0) :
+            echo "Há $vehiclesInParkingLot veículo(s) estacionado(s) no momento.";
+        else :
+            echo "Não há veículos estacionados no momento.";
+        endif;
+     }
+
    
    
      private function setQtdVehiclesInParkingLot(int $action):void  
@@ -117,16 +138,21 @@
      }
 }  
  
- 
+$parkLot = new ParkingLot(-3,100);
+$parkLot->howManyVehiclesAreInParkingLot(); 
+echo "<br>";
 // $parkLot = new ParkingLot(3);
-// echo $parkLot->getQtdOfParkingLots(); 
+$parkLot->limitOfVehicles(); 
 // echo "<br>"; 
-// $parkLot->toParkVehicle();
-// echo "<br>"; 
+$parkLot->toParkVehicle();
+echo "<br>"; 
 // echo $parkLot->getQtdOfVehiclesInParkingLot();
 // echo "<br>"; 
-// $parkLot->toParkVehicle();
-// echo "<br>"; 
+$parkLot->toParkVehicle();
+echo "<br>"; 
+
+$parkLot->howManyVehiclesAreInParkingLot(); 
+echo "<br>";
 // $parkLot->toParkVehicle();
 // echo "<br>"; 
 // $parkLot->toParkVehicle(); 
@@ -135,8 +161,8 @@
 // echo "<br>"; 
 // $parkLot->areThereFreeParkingLots();
 // echo "<br>Faturamento"; 
-// $parkLot->showInvoicing();
-// echo "<br>"; 
+$parkLot->showInvoicing();
+echo "<br>"; 
 // $parkLot->getOutOfTheParkVehicle();
 // echo "<br>"; 
 // $parkLot->areThereFreeParkingLots();
@@ -149,20 +175,23 @@
 // echo "<br>"; 
 // echo $parkLot->getQtdOfVehiclesInParkingLot();
 // echo "<br>"; 
-// echo $parkLot->expansionParkingLot(4);
-// echo "<br>"; 
+echo $parkLot->expansionParkingLot(4);
+echo "<br>";
+$parkLot->limitOfVehicles(); 
+echo $parkLot->expansionParkingLot(-4);
+echo "<br>";
 // echo $parkLot->getQtdOfParkingLots();
 // echo "<br>"; 
 // $parkLot->areThereFreeParkingLots();
 // echo "<br>"; 
-// $parkLot->toParkVehicle();
-// echo "<br>"; 
-// $parkLot->toParkVehicle();
-// echo "<br>"; 
-// $parkLot->areThereFreeParkingLots();
-// echo "<br>"; 
-// $parkLot->showInvoicing();
-// echo "<br>"; 
+$parkLot->toParkVehicle();
+echo "<br>"; 
+$parkLot->toParkVehicle();
+echo "<br>"; 
+$parkLot->areThereFreeParkingLots();
+echo "<br>"; 
+$parkLot->showInvoicing();
+echo "<br>"; 
 // echo $parkLot->getQtdOfVehiclesInParkingLot();
 // echo "<br>"; 
 // $parkLot->getOutOfTheParkVehicle();
--- ParkingLot/readme.md
+++ ParkingLot/readme.md
@@ -1,16 +1,14 @@
-Iniciando os trabalhos
-
-* Comece criando um novo ParkingLot :
-    → Você pode deve definir quantas vagas o estacionamento tem!
+* Comece criando um novo Estacionamento:
+    → $nomeDoEstacionamento = new ParkingLot($numeroVagas, $valorParaEstacionar)
+    → Você pode definir quantas vagas tem, caso não informar será criado um estacionamento com 1 vaga apenas
     → O valor que deve ser pago pelos veículos pode ser informado ou não, caso não for, é cobrado R$ 5,00
-* Funções para informações do estacionamento que devem ser usadas com comando echo:
-    → getQtdOfParkingLots() - Quantidade de vagas
-    → getQtdOfVehiclesInParkingLot() - Quantidade de veículos estacionados
-
+ 
 * Ações que podem ser feitas:
+    → limitOfVehicles() - Verificar a quantidade de vagas do estacionameto
     → toParkVehicle() - Estacionar um veículo;
-    → getOutOfTheParkVehicle() - Veículo sair do estacionamento;
+    → getOutOfTheParkVehicle() - Veículo sai do estacionamento;
     → areThereFreeParkingLots() - Verificar se há vagas no estacionamento;
     → showInvoicing() - Ver o faturamento atual do estacionamento;
-    → expansionParkingLot(int $newQtdOfParkingLots) - Expandir o estacionamento
+    → expansionParkingLot(int $newQtdOfParkingLots) - Expandir o estacionamento;
+    → howManyVehiclesAreInParkingLot() - Quantidade de veículos estacionados;
 
