475c134ee69468a75aaaf2a0fcfe13387829011c
Lucas Tonolli <tonollilucas@yahoo.com>
2022-09-03 11:46:53 -0300

    Funcionado da forma errada

--- hash-game/hash-game.js
+++ hash-game/hash-game.js
@@ -33,43 +33,41 @@ function checkTheHashGame() {
         return [element.id, element.innerHTML];
     });
 
-    console.log(positionGame);
-    // for (i = 0; i < 9; i += 3) {
-    //     if (positionGame[i].innerHTML === positionGame[i + 1].innerHTML && positionGame[i + 2].innerHTML){
-    //         console.log(i);
-    //         console.log(positionGame[i].innerHTML +' - ' + i);
-    //         console.log(positionGame[i + 1].innerHTML +' - ' + (i + 1));
-    //         console.log(positionGame[i + 2].innerHTML +' - ' + (i + 2));
-    //         alert(positionGame[i].innerHTML + " Ganhou 1")
-    //         clearHashGame();
-    //         return;
-    //     }
-    // }
-    // for (i = 0; i < 3; i ++){
-    //     if (positionGame[i].innerHTML === positionGame[i + 3].innerHTML && positionGame[i + 6].innerHTML){
-
-    //             console.log(positionGame[i].innerHTML +' - ' + i);
-    //             console.log(positionGame[i + 3].innerHTML +' - ' + (i + 3));
-    //             console.log(positionGame[i + 6].innerHTML +' - ' + (i + 6));
-    //             alert(positionGame[i].innerHTML + " Ganhou 2")
-    //             clearHashGame();
-    //             return;
-    //         }
-    // }
-    // for (i = 0; i < 3; i +=2){
-    //     if ((positionGame[i].innerHTML === positionGame[i + 4].innerHTML && positionGame[i + 8].innerHTML) || (positionGame[i].innerHTML === positionGame[i + 2].innerHTML && positionGame[i + 4].innerHTML)){
-    //         // console.log(positionGame[i].innerHTML);
-    //         // console.log(positionGame[i + 1].innerHTML);
-    //         // console.log(positionGame[i + 2].innerHTML);
-    //         console.log(i);
-    //         alert(positionGame[i].innerHTML + " Ganhou 3")
-    //         clearHashGame();
-    //         return;
-    //     }
-    // }
-    
+    // console.log(positionGame);
+    for (i = 0; i < 9; i++){
         
-    
+        if (positionGame[i][1] === "") 
+            continue;
+        
+        if (i == 0 || i == 3 || i == 6)
+            if (positionGame[i][1] === positionGame[i + 1][1] && positionGame[i][1] === positionGame[i + 2][1] ){
+                alert(positionGame[i][1] + " Ganhou!");
+                clearHashGame();
+                return
+            }
+        if (i == 0 || i == 1 || i == 2)
+            if (positionGame[i][1] === positionGame[i + 3][1] && positionGame[i][1] === positionGame[i + 6][1] ){
+                alert(positionGame[i][1] + " Ganhou!");
+                clearHashGame();
+                return
+            }
+        if (i == 0)
+            if (positionGame[i][1] === positionGame[i + 4][1] && positionGame[i][1] === positionGame[i + 8][1] ){
+                alert(positionGame[i][1] + " Ganhou!");
+                clearHashGame();
+                return
+            }
+        if (i == 2)
+            if (positionGame[i][1] === positionGame[i + 2][1] && positionGame[i][1] === positionGame[i + 4][1] ){
+                alert(positionGame[i][1] + " Ganhou!");
+                clearHashGame();
+                return
+            }
+    }
+    if (play === 9){
+        alert("Deu velha!")
+        clearHashGame();
+    }
 }
 
 function clearHashGame(){
